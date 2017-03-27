675967efcd15904051d219f432eddcb9632723df
Eranimor <alexander.moeller@ein-reisender.de>
2017-03-27 21:20:22 +0200

    Immer noch Button Tooltip. Einige Dateien felten.

---
+++ Assets/_Scripts/displayButtonDescription.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class displayButtonDescription : MonoBehaviour
+{
+    public string _ButtonDescription;
+    public Text _DescriptionField;
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("OnMouseEnter"); 
+        _DescriptionField.text = _ButtonDescription;
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("OnMouseExit");
+        _DescriptionField.text = "";
+    }
+}
